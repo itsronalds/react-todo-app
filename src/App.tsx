34d56209c7ab01fc0e199b1516a73a560658cde8
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2023-08-19 14:45:42 -0400

    Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState<string[]>([]);
+  const [item, setItem] = useState<string>('');
+
+  const add = () => {
+    if (item === '') {
+      return alert('Item name is required.');
+    }
+
+    setItems((state) => [...state, item]);
+    setItem('');
+  };
+
+  const remove = (itemName: string) => {
+    const newItems = items.filter((item) => item !== itemName);
+    setItems(newItems);
+  };
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setItem(value as string);
+  };
+
+  // Render logic
+  const listItems =
+    items.length === 0 ? (
+      <h2>Ups, No items registered</h2>
+    ) : (
+      items?.map((item, index) => (
+        <div key={item} className="item" onClick={() => remove(item)}>
+          {index + 1} - {item}
+        </div>
+      ))
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="list">
+        <h1>List of Items</h1>
+        <hr />
+        <div className="items">{listItems}</div>
+        <div>
+          <input type="text" placeholder="Enter item name..." value={item} onChange={onChange} />
+          <button type="button" onClick={add}>
+            Add item
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
